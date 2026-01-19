359fa207c9786f5067eb2b46a168e4dc3c7acc7d
alexis zbik <alexis@bleass.com>
2026-01-19 18:04:34 +0100

    Yay RAM

--- MIDIPrint.ino
+++ MIDIPrint.ino
@@ -11,6 +11,7 @@
 #include "Adafruit_Thermal.h"
 
 #include <MIDI.h>
+#include <avr/pgmspace.h>
 
 const byte imageBeginIdx = 5;
 const int imageMaxSize = 192 * 3;
@@ -29,17 +30,52 @@ struct ArtistText {
     const char* link;
 };
 
-ArtistText ymnk_ = {"YMNK incarne une musique\nélectronique organique et\ndéjantée où sa casquette\nd'ingénieur-bricoleur est mise\nau service de sa création. Seul\nsur scène, armé de ses claviers\nmodulaires fait maison et d'une\nguitare électrique aux airs\nhéroïques, le\nmulti-instrumentiste propose un\nlive de haute volée avec un\nset-up à son image qui ne laisse\npersonne indifférent.", "https://www.instagram.com/ymnkmusic"};
-ArtistText douran_ = {"Douran tisse un dialogue subtil\nentre musique orchestrale et\nélectronique, créant une\nexpérience émotionnelle, intense\net cinématographique. Ses\ncompositions mêlent pianos,\ncordes et synthétiseurs avec\nfinesse pour façonner un univers\nà la fois épique, délicat et\npuissant. Après 4 EPs, plus de\n10 millions d'écoutes, des\nmusiques à l'image, l'artiste\ndévoile un double EP de 14\ntitres : 7 compositions\noriginales avec chœur bulgare,\nquatuor à cordes et synthés\nanalogiques, et 7 remixes.\nSortie finale le 13 mars au\nHasard Ludique à Paris, avec un\nlive inédit accompagné d'un\nbatteur.", "https://open.spotify.com/intl-fr/artist/3Rvdn0CASBunaYXQHEKIoQ"};
-ArtistText deadChic_ = {"Une voix de velours éraillée,\nune guitare à la mélodie\nrugueuse, l'alliance rock\nfranco-anglaise que symbolise\nDead Chic est d'une classe\nsubtile et sauvage. Le groupe\namené par Andy Balcon\n(ex-Heymoonshaker) et Damien\nFélix livre des compositions\nriches et généreuses pour un\nlive où les 4 comparses\ndéroulent une folie rock\ncinématographique et\nincantatoire. La proximité du\ngroupe avec le public nous\nplonge dans un univers bien à\neux, à la fois sombre et chic !", "https://open.spotify.com/artist/4hCjCdMZF65nKTdjsPUvfh"};
-ArtistText nouvelleVague_ = {"Nouvelle Vague est un groupe\nfrançais formé en 2003 par Marc\nCollin et Olivier Libaux. Il\nréinvente le punk, le post-punk\net la new wave des années 70-80\nen versions acoustiques et bossa\nnova, avec des reprises chantées\nprincipalement par des voix\nféminines. Leur style\nminimaliste, rétro et élégant\ntransforme des classiques en\nsons doux et atmosphériques,\nséduisant un public\ninternational. Connus pour leur\napproche artistique, ils ont\nmarqué la scène française et\nmondiale par cette fusion\naudacieuse de nostalgie et\nmodernité. Nouvel album en 2027", "https://open.spotify.com/artist/4h7NLIlg1oYdEtfQJfyto0"};
-ArtistText delaurentis_ = {"Souvent qualifiée\n d'\"électronicienne\", \nDeLaurentis dépasse\nlargement cette\nétiquette. Avec Musicalism, son\nalbum le plus abouti, elle mêle\nexigence savante et énergie\ndansante. Elle explore une\nmusique où sons, couleurs et\némotions fusionnent par la\nsynesthésie. Entourée de\ncollaborations fortes et d'une\nlutherie digitale innovante\n(Erae, IA, gants connectés),\nDeLaurentis compose une musique\ndu futur, immersive et\nprofondément sensible. Le 15\navril, l'artiste se produira au\nPrintemps de Bourges pour une\ncréation spéciale célébrant les\n50 ans du festival, mettant en\nlumière les pionnières de la\nmusique électronique.", "https://open.spotify.com/intl-fr/artist/1Km7DPrZEzlSllBzzkCSN8"};
-ArtistText sonia_ = {"Entrez dans SON'IA 360,\nl'expérience électro qui vous\nentoure totalement. DOURAN,\nDELAURENTIS et YMNK combinent\nstéréo, A/V 360, et 3D , avec\nHervé Déjardin de Radio France\naux mix spatial pour faire\nvibrer chaque spectateur. Plus\nqu'un concert, un voyage\nmultidimensionnel où chaque\nnote, chaque mouvement crée une\nconnexion intime et collective,\nredéfinissant le live comme\njamais. Nous fournissons les 12\npoints de diffusion, en\npartenariat avec Nexo de Yamaha.", "https://www.france.tv/spectacles-et-culture/6776809-delaurentis-live-360-a-la-gaite-lyrique.html"};
-ArtistText ilEstVilaine_ = {"Quatre ans après Les Mystères de\nLorient, Il Est Vilaine signe\nson retour avec Best Service, un\nalbum foisonnant et cosmopolite\nattendu le 18 septembre 2026.\nDans une époque assombrie, le\ngroupe élargit les horizons et\nfaçonne un univers sonore\nhybride, où se croisent\nélectronique analogique, techno,\nrock vintage et synth-pop. Ces\npaysages intenses, traversés\nd'énergie brute, de groove et\nd'un romantisme sombre, offrent\nun terrain de jeu idéal à des\ncollaborations aussi audacieuses\nque inattendues. Release party :\n13 octobre 2026 - La\nMaroquinerie (Paris).", "https://open.spotify.com/intl-fr/artist/7mUEGrKLGWDJFXX9odcxMM"};
-ArtistText prettyInside_ = {"Pretty Inside est un groupe\nbordelais mené par Alexis\nDeux-Seize (membre actif du\ncollectif Flippin' Freaks et\nbassiste d'Opinion). Le groupe\nalterne des brûlots rock garage\net indus, des perles pop\nshoegaze, et un songwriting\nexistentiel et cathartique\ninvoquant des références\néminemment 90's comme plus\nactuelles dont l'homogénéité\nréside dans un chant honnête et\npuissant de spontanéité. Le\nnouvel album du groupe Ever\nGonna Heal sorti le 12 décembre\n2025, est fruit d'un long et\néprouvant processus humain,\namenant le groupe vers une\nidentité singulière et assumée.", "https://open.spotify.com/intl-fr/artist/0NUfUzPh4iA8KZWaEP8MH0"};
-ArtistText elvett_ = {"Elvett propose une formation\nautour d'un piano à queue,\nportée par des voix féminines.\nAncré dans une pop exigeante, le\nsextet mêle sons électroniques\net analogiques, chants\ntraditionnels et piano lyrique\nou tribal, pour un voyage\nintrospectif et intense.\nVéritable écrin pour la voix\nsolaire de Lyn M., il explore\nune soul authentique tout en\ninnovant grâce aux productions\nlumineuses d'Alain Frey. Le\nnouvel album sortira le 26\nfévrier 2027, un spectacle\norchestral avec cuivres sera\nprésenté en collaboration avec\nvos conservatoires.", "https://open.spotify.com/intl-fr/artist/4jTcePhvBRxrOONkmpm5WX"};
-ArtistText bran_ = {"Sur scène, BV3 Elektrio revisite\nles morceaux phares du\nrépertoire Bran Van, insufflant\nune énergie moderne et dansante\nà des classiques qui n’ont rien\nperdu de leur pouvoir\nrassembleur. Mais la proposition\nne se limite pas à la nostalgie\n: de toutes nouvelles\ncompositions, jusqu'ici\nsoigneusement gardées secrètes\npar Di Salvio, viendront\nenrichir le spectacle. Le\nrésultat : un live incandescent,\nporté par des grooves\nhypnotiques, des nappes\nélectroniques et une complicité\nmusicale qui rappellent combien\nBran Van 3000 a toujours su\ntraverser les époques.", "https://open.spotify.com/artist/3D3blX3lLE3BCMdo3SOMlB"};
-ArtistText vss_ = {"Vipères Sucrées Salées crache un\nrock nerveux et tranchant, riffs\nacérés, énergie brute et textes\nen français qui frappent. Chaque\nlive est un uppercut sonore, un\nmaelström électrique qui emporte\nle public. Du trio au quintet,\nil prépare déjà la suite,\npromettant de nouveaux morceaux\naussi intenses et captivants que\nleur premier chaos sonore.", "https://open.spotify.com/intl-fr/album/2c6Y0gdj5J23TeQhEHJwq2"};
+// Tous les textes stockés en PROGMEM (Flash) pour économiser la RAM
+const char ymnk_bio[] PROGMEM = "YMNK incarne une musique\nélectronique organique et\ndéjantée où sa casquette\nd'ingénieur-bricoleur est mise\nau service de sa création. Seul\nsur scène, armé de ses claviers\nmodulaires fait maison et d'une\nguitare électrique aux airs\nhéroïques, le\nmulti-instrumentiste propose un\nlive de haute volée avec un\nset-up à son image qui ne laisse\npersonne indifférent.";
+const char ymnk_link[] PROGMEM = "https://www.instagram.com/ymnkmusic";
+
+const char douran_bio[] PROGMEM = "Douran tisse un dialogue subtil\nentre musique orchestrale et\nélectronique, créant une\nexpérience émotionnelle, intense\net cinématographique. Ses\ncompositions mêlent pianos,\ncordes et synthétiseurs avec\nfinesse pour façonner un univers\nà la fois épique, délicat et\npuissant. Après 4 EPs, plus de\n10 millions d'écoutes, des\nmusiques à l'image, l'artiste\ndévoile un double EP de 14\ntitres : 7 compositions\noriginales avec chœur bulgare,\nquatuor à cordes et synthés\nanalogiques, et 7 remixes.\nSortie finale le 13 mars au\nHasard Ludique à Paris, avec un\nlive inédit accompagné d'un\nbatteur.";
+const char douran_link[] PROGMEM = "https://open.spotify.com/intl-fr/artist/3Rvdn0CASBunaYXQHEKIoQ";
+
+const char deadChic_bio[] PROGMEM = "Une voix de velours éraillée,\nune guitare à la mélodie\nrugueuse, l'alliance rock\nfranco-anglaise que symbolise\nDead Chic est d'une classe\nsubtile et sauvage. Le groupe\namené par Andy Balcon\n(ex-Heymoonshaker) et Damien\nFélix livre des compositions\nriches et généreuses pour un\nlive où les 4 comparses\ndéroulent une folie rock\ncinématographique et\nincantatoire. La proximité du\ngroupe avec le public nous\nplonge dans un univers bien à\neux, à la fois sombre et chic !";
+const char deadChic_link[] PROGMEM = "https://open.spotify.com/artist/4hCjCdMZF65nKTdjsPUvfh";
+
+const char nouvelleVague_bio[] PROGMEM = "Nouvelle Vague est un groupe\nfrançais formé en 2003 par Marc\nCollin et Olivier Libaux. Il\nréinvente le punk, le post-punk\net la new wave des années 70-80\nen versions acoustiques et bossa\nnova, avec des reprises chantées\nprincipalement par des voix\nféminines. Leur style\nminimaliste, rétro et élégant\ntransforme des classiques en\nsons doux et atmosphériques,\nséduisant un public\ninternational. Connus pour leur\napproche artistique, ils ont\nmarqué la scène française et\nmondiale par cette fusion\naudacieuse de nostalgie et\nmodernité. Nouvel album en 2027";
+const char nouvelleVague_link[] PROGMEM = "https://open.spotify.com/artist/4h7NLIlg1oYdEtfQJfyto0";
+
+const char delaurentis_bio[] PROGMEM = "Souvent qualifiée\n d'\"électronicienne\", \nDeLaurentis dépasse\nlargement cette\nétiquette. Avec Musicalism, son\nalbum le plus abouti, elle mêle\nexigence savante et énergie\ndansante. Elle explore une\nmusique où sons, couleurs et\némotions fusionnent par la\nsynesthésie. Entourée de\ncollaborations fortes et d'une\nlutherie digitale innovante\n(Erae, IA, gants connectés),\nDeLaurentis compose une musique\ndu futur, immersive et\nprofondément sensible. Le 15\navril, l'artiste se produira au\nPrintemps de Bourges pour une\ncréation spéciale célébrant les\n50 ans du festival, mettant en\nlumière les pionnières de la\nmusique électronique.";
+const char delaurentis_link[] PROGMEM = "https://open.spotify.com/intl-fr/artist/1Km7DPrZEzlSllBzzkCSN8";
+
+const char sonia_bio[] PROGMEM = "Entrez dans SON'IA 360,\nl'expérience électro qui vous\nentoure totalement. DOURAN,\nDELAURENTIS et YMNK combinent\nstéréo, A/V 360, et 3D , avec\nHervé Déjardin de Radio France\naux mix spatial pour faire\nvibrer chaque spectateur. Plus\nqu'un concert, un voyage\nmultidimensionnel où chaque\nnote, chaque mouvement crée une\nconnexion intime et collective,\nredéfinissant le live comme\njamais. Nous fournissons les 12\npoints de diffusion, en\npartenariat avec Nexo de Yamaha.";
+const char sonia_link[] PROGMEM = "https://www.france.tv/spectacles-et-culture/6776809-delaurentis-live-360-a-la-gaite-lyrique.html";
+
+const char ilEstVilaine_bio[] PROGMEM = "Quatre ans après Les Mystères de\nLorient, Il Est Vilaine signe\nson retour avec Best Service, un\nalbum foisonnant et cosmopolite\nattendu le 18 septembre 2026.\nDans une époque assombrie, le\ngroupe élargit les horizons et\nfaçonne un univers sonore\nhybride, où se croisent\nélectronique analogique, techno,\nrock vintage et synth-pop. Ces\npaysages intenses, traversés\nd'énergie brute, de groove et\nd'un romantisme sombre, offrent\nun terrain de jeu idéal à des\ncollaborations aussi audacieuses\nque inattendues. Release party :\n13 octobre 2026 - La\nMaroquinerie (Paris).";
+const char ilEstVilaine_link[] PROGMEM = "https://open.spotify.com/intl-fr/artist/7mUEGrKLGWDJFXX9odcxMM";
+
+const char prettyInside_bio[] PROGMEM = "Pretty Inside est un groupe\nbordelais mené par Alexis\nDeux-Seize (membre actif du\ncollectif Flippin' Freaks et\nbassiste d'Opinion). Le groupe\nalterne des brûlots rock garage\net indus, des perles pop\nshoegaze, et un songwriting\nexistentiel et cathartique\ninvoquant des références\néminemment 90's comme plus\nactuelles dont l'homogénéité\nréside dans un chant honnête et\npuissant de spontanéité. Le\nnouvel album du groupe Ever\nGonna Heal sorti le 12 décembre\n2025, est fruit d'un long et\néprouvant processus humain,\namenant le groupe vers une\nidentité singulière et assumée.";
+const char prettyInside_link[] PROGMEM = "https://open.spotify.com/intl-fr/artist/0NUfUzPh4iA8KZWaEP8MH0";
+
+const char elvett_bio[] PROGMEM = "Elvett propose une formation\nautour d'un piano à queue,\nportée par des voix féminines.\nAncré dans une pop exigeante, le\nsextet mêle sons électroniques\net analogiques, chants\ntraditionnels et piano lyrique\nou tribal, pour un voyage\nintrospectif et intense.\nVéritable écrin pour la voix\nsolaire de Lyn M., il explore\nune soul authentique tout en\ninnovant grâce aux productions\nlumineuses d'Alain Frey. Le\nnouvel album sortira le 26\nfévrier 2027, un spectacle\norchestral avec cuivres sera\nprésenté en collaboration avec\nvos conservatoires.";
+const char elvett_link[] PROGMEM = "https://open.spotify.com/intl-fr/artist/4jTcePhvBRxrOONkmpm5WX";
+
+const char bran_bio[] PROGMEM = "Sur scène, BV3 Elektrio revisite\nles morceaux phares du\nrépertoire Bran Van, insufflant\nune énergie moderne et dansante\nà des classiques qui n'ont rien\nperdu de leur pouvoir\nrassembleur. Mais la proposition\nne se limite pas à la nostalgie\n: de toutes nouvelles\ncompositions, jusqu'ici\nsoigneusement gardées secrètes\npar Di Salvio, viendront\nenrichir le spectacle. Le\nrésultat : un live incandescent,\nporté par des grooves\nhypnotiques, des nappes\nélectroniques et une complicité\nmusicale qui rappellent combien\nBran Van 3000 a toujours su\ntraverser les époques.";
+const char bran_link[] PROGMEM = "https://open.spotify.com/artist/3D3blX3lLE3BCMdo3SOMlB";
+
+const char vss_bio[] PROGMEM = "Vipères Sucrées Salées crache un\nrock nerveux et tranchant, riffs\nacérés, énergie brute et textes\nen français qui frappent. Chaque\nlive est un uppercut sonore, un\nmaelström électrique qui emporte\nle public. Du trio au quintet,\nil prépare déjà la suite,\npromettant de nouveaux morceaux\naussi intenses et captivants que\nleur premier chaos sonore.";
+const char vss_link[] PROGMEM = "https://open.spotify.com/intl-fr/album/2c6Y0gdj5J23TeQhEHJwq2";
+
+// Structures pointant vers PROGMEM
+ArtistText ymnk_ = {ymnk_bio, ymnk_link};
+ArtistText douran_ = {douran_bio, douran_link};
+ArtistText deadChic_ = {deadChic_bio, deadChic_link};
+ArtistText nouvelleVague_ = {nouvelleVague_bio, nouvelleVague_link};
+ArtistText delaurentis_ = {delaurentis_bio, delaurentis_link};
+ArtistText sonia_ = {sonia_bio, sonia_link};
+ArtistText ilEstVilaine_ = {ilEstVilaine_bio, ilEstVilaine_link};
+ArtistText prettyInside_ = {prettyInside_bio, prettyInside_link};
+ArtistText elvett_ = {elvett_bio, elvett_link};
+ArtistText bran_ = {bran_bio, bran_link};
+ArtistText vss_ = {vss_bio, vss_link};
 
 unsigned char show[] = {
   0x1B, 0x2A, 0x20, 0xC0, 0x00,  //Bitmap size: 24*192, revise parameter with the reference of general instruction set 34 and 35.
@@ -265,7 +301,14 @@ Adafruit_Thermal printer(&mySerial);      // Pass addr to printer constructor
 // -----------------------------------------------------------------------
 
 void printQR(const char* text) {
-  uint8_t len = strlen(text) + 3;
+  // Calculer la longueur en lisant depuis PROGMEM
+  uint8_t len = 0;
+  const char* p = text;
+  while (pgm_read_byte(p) != 0) {
+    len++;
+    p++;
+  }
+  len += 3;
 
   mySerial.write(0x1D);
   mySerial.write(0x28);
@@ -275,7 +318,13 @@ void printQR(const char* text) {
   mySerial.write(0x31);
   mySerial.write(0x50);
   mySerial.write(0x30);
-  mySerial.print(text);
+  
+  // Imprimer depuis PROGMEM
+  p = text;
+  while (pgm_read_byte(p) != 0) {
+    mySerial.write(pgm_read_byte(p));
+    p++;
+  }
 
   mySerial.write(0x1B);
   mySerial.write(0x61);
@@ -291,13 +340,13 @@ void printQR(const char* text) {
 }
 
 void printAsciiFun(const char* txt) {
-  int count = 0;
-  while (*txt) {
-    uint8_t c = (uint8_t)*txt++;
+  const char* p = txt;
+  while (pgm_read_byte(p) != 0) {
+    uint8_t c = pgm_read_byte(p++);
 
     // UTF-8 accented characters start with 0xC3
-    if (c == 0xC3 && *txt) {
-      uint8_t next = (uint8_t)*txt++;
+    if (c == 0xC3 && pgm_read_byte(p) != 0) {
+      uint8_t next = pgm_read_byte(p++);
 
       switch (next) {
 
@@ -348,14 +397,14 @@ void printAsciiFun(const char* txt) {
     }
 
     // œ
-    else if (c == 0xC5 && *txt == 0x93) {
-      txt++;
+    else if (c == 0xC5 && pgm_read_byte(p) == 0x93) {
+      p++;
       printer.print("oe");
     }
 
     // æ
-    else if (c == 0xC3 && *txt == 0xA6) {
-      txt++;
+    else if (c == 0xC3 && pgm_read_byte(p) == 0xA6) {
+      p++;
       printer.print("ae");
     }
 
@@ -390,29 +439,9 @@ void setup() {
   printer.feed(2);
 
   // Select code page CP1252 (Latin-1 occidental)
-mySerial.write(0x1B);
-mySerial.write(0x74);
-mySerial.write(0x10); // 0x10 = CP1252 (selon firmware)
-
-  printArtist(&ymnk_);
-  delay(10);
-  printArtist(&douran_);
-  delay(10);
-  printArtist(&deadChic_);
-  delay(10);
-  printArtist(&nouvelleVague_);
-  delay(10);
-  printArtist(&delaurentis_);
-  delay(10);
-  printArtist(&sonia_);
-  delay(10);
-  printArtist(&ilEstVilaine_);
-  delay(10);
-  printArtist(&elvett_);
-  delay(10);
-  printArtist(&bran_);
-  delay(10);
-  printArtist(&vss_);
+  mySerial.write(0x1B);
+  mySerial.write(0x74);
+  mySerial.write(0x10); // 0x10 = CP1252 (selon firmware)
 }
 
 
@@ -434,7 +463,20 @@ void handleControlChange(byte channel, byte control, byte value) {
 }
 
 void handleProgramChange(byte channel, byte program) {
-
+  switch(program) {
+    case 1 :  printArtist(&ymnk_); break;
+    case 2 :  printArtist(&douran_); break;
+    case 3 :  printArtist(&deadChic_); break;
+    case 4 :  printArtist(&nouvelleVague_); break;
+    case 5 :  printArtist(&delaurentis_); break;
+    case 6 :  printArtist(&sonia_); break;
+    case 7 :  printArtist(&ilEstVilaine_); break;
+    case 8 :  printArtist(&elvett_); break;
+    case 9 :  printArtist(&bran_); break;
+    case 10 : printArtist(&vss_); break;
+    case 11 : printArtist(&prettyInside_); break;
+    default : break;
+  }
 }
 
 void handleNoteOn(byte channel, byte note, byte velocity) {
@@ -474,7 +516,7 @@ void printArtist(ArtistText* artist) {
   printer.feed(1);
   printQR(artist->link);
   mySerial.print("--------------------------------");
-  printer.feed(1);
+  printer.feed(2);
   printer.sleep();       // Tell printer to sleep
   delay(1000L);          
   printer.wake();        // MUST wake() before printing again, even if reset
